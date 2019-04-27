caefa8149a6fe3361f1698ac493c38587a0cb187
neripark <neripark@gmail.com>
2019-04-28 04:10:07 +0900

    見出しコンポーネントの追加、プロフィールに追加

---
+++ components/Heading.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+h2.heading {{ text }}
+</template>
+
+<script>
+export default {
+  props: ['text', 'color']
+}
+</script>
+
+<style lang="scss" scoped>
+.heading {
+  position: absolute;
+  top: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: $color-main-blue;
+  letter-spacing: 1em;
+  text-indent: 1em;
+  font-size: 48px;
+  text-align: center;
+}
+</style>
--- components/Profile.vue
+++ components/Profile.vue
@@ -1,14 +1,47 @@
 <template lang="pug">
 //- todo: 画像の余白調整
 .profile
-  img.profile-photo(
-    src="~assets/images/profile.jpg"
-    alt="profile photo"
+  heading(
+    text="Profile"
+    color="green"
   )
+  .wrap-photo
+    img.profile-photo(
+      src="~assets/images/profile.jpg"
+      alt="profile photo"
+    )
 </template>
 
+<script>
+import Heading from '~/components/Heading.vue'
+export default {
+  components: {
+    Heading
+  }
+}
+</script>
+
 <style lang="scss" scoped>
+.profile {
+  position: relative;
+}
+.wrap-photo {
+  min-height: 630px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+}
 .profile-photo {
   width: 100%;
+  min-width: 1500px;
+  margin: 0 auto;
+  margin-top: -280px;
+}
+// todo: media_queryをmixin化
+@media (min-width: 1500px) {
+  .profile-photo {
+    height: 100%;
+    margin-top: -350px;
+  }
 }
 </style>

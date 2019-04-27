d0e8ce251da6ea8d4e48792987af45d0fcda05e9
neripark <neripark@gmail.com>
2019-04-28 01:35:53 +0900

    FloatingInfo追加

---
+++ components/FloatingInfo.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.floating-info
+  .wrap
+    section.info-unit
+      h3.head News
+      p.lead
+        | りんご音楽祭出演をかけたオーディション "RINGOOOO A GO-GO" に出場します！
+        br
+        | http://ringofes.info/ringooo-a-go-go-schedule/
+    section.info-unit
+      h3.head Next Live
+      p.lead
+        | 2019/6/8 (Sat.)
+        br
+        | なんかすごそうなイベント名@Shimokitazawa ERA
+        br
+        | w) mouse on the keys / toe / AGRAPH
+</template>
+
+<style lang="scss" scoped>
+.floating-info {
+  position: absolute;
+  width: 100%;
+  max-width: 1500px;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  color: #fff;
+}
+.wrap {
+  width: 300px;
+  padding: 30px;
+}
+.info-unit {
+  &:not(:first-child) {
+    margin-top: 20px;
+  }
+}
+.head {
+  font-size: 24px;
+}
+.lead {
+  margin-top: 10px;
+  font-size: 16px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 main.container
   MainVisual
+  FloatingInfo
   SiteNav
   Profile
   MusicVideo
@@ -22,10 +23,12 @@ import MainVisual from '~/components/MainVisual.vue'
 import SiteNav from '~/components/SiteNav'
 import Profile from '~/components/Profile'
 import MusicVideo from '~/components/MusicVideo'
+import FloatingInfo from '~/components/FloatingInfo'
 
 export default {
   components: {
     MainVisual,
+    FloatingInfo,
     SiteNav,
     Profile,
     MusicVideo

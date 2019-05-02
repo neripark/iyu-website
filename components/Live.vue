4b2794d3a1a7be7c5e8d5d285678e3f1525ac14c
neripark <neripark@gmail.com>
2019-05-02 15:55:47 +0900

    Live欄に予約への導線を追加

--- components/Live.vue
+++ components/Live.vue
@@ -5,23 +5,50 @@
     color="yellow"
   )
   p.lead
-    | チケットのお取置きはこちらのメールフォームかtwitterまでお願い致します。
+    | チケットのお取置きは
+    a.contact(:href="`#${anchorList.profile.id}`" v-scroll-to="scrollTo(anchorList.contact)") こちらのフォーム
+    | から。
+    br
+    a.twitter(
+      href="https://twitter.com/iyu_band"
+      target="_blank"
+    ) Twitter
+    | でも承っております。
   live-cards
 </template>
 
 <script>
 import Heading from '~/components/Heading.vue'
 import LiveCards from '~/components/LiveCards.vue'
+import { anchorList, scrollOptions } from '~/assets/js/v-scroll-settings.js'
 
 export default {
   components: {
     Heading,
     LiveCards
+  },
+  data() {
+    return {
+      anchorList: anchorList
+    }
+  },
+  methods: {
+    scrollTo(to) {
+      return {
+        duration: scrollOptions.duration,
+        easing: scrollOptions.easing,
+        el: `#${to.id}`,
+        offset: to.offset
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+* {
+  color: $white;
+}
 .live {
   background: $main-blue;
   padding-bottom: 50px;
@@ -31,8 +58,14 @@ export default {
   }
 }
 .lead {
-  color: $white;
   text-align: center;
   line-height: 2;
+  @include mq() {
+    line-height: 1.5;
+  }
+}
+.contact,
+.twitter {
+  text-decoration: underline;
 }
 </style>

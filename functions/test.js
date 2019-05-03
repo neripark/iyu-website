fa04d0ef15fc80140a39f7c92576da15f856ac8e
neripark <neripark@gmail.com>
2019-05-03 19:12:03 +0900

    update test function.

--- functions/test.js
+++ functions/test.js
@@ -1,1 +1,9 @@
-console.log("Netlify function test!");
+module.handler = function(event, context, testConsole) {
+  return {
+    testMessage: "this is test."
+  };
+}
+
+function testConsole() {
+  return "test message.";
+}

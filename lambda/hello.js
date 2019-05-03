8bebf622cffa6380a6e54792fae584a6009cceb7
neripark <neripark@gmail.com>
2019-05-03 20:10:03 +0900

    init build setting for lambda functions.

---
+++ lambda/hello.js
@@ -0,0 +1,8 @@
+exports.handler = function(event, context, callback) {
+  const eventString = JSON.stringify(event);
+  callback(null, {
+    statusCode: 200,
+    // body: 'Hello!!!!!'
+    body: eventString
+  })
+}

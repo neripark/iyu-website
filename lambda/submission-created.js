6c836868d8dce1aa4e32aeec0d2215540092737d
neripark <neripark@gmail.com>
2019-05-05 12:14:27 +0900

    success decoding by querystring.parse

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -5,28 +5,25 @@ exports.handler = function(event, context, callback) {
   const token = 'sFW0U11C8weGFxvGsnL8MMXG0aT3ta7fpqvSc2SHbRU'
   // const payload = event.body.payload
 
-  const payload = typeof event.body // これはString
-  const payload2 = typeof event.body.payload // これはundefined!
+  // const payload = typeof event.body // これはString
+  // const payload2 = typeof event.body.payload // これはundefined!
 
-  // const msg = new Buffer(event.body, 'base64')
 
-//   const msg = ` // [Name] ${payload.name}
-// [Category] ${payload.category}
-// [LiveDate] ${payload.livedate}
-// [TicketsCount] ${payload.ticketscount}
-// [email] ${payload.email}
-// [Content]
-// ${payload.content}
-//   `
+  const params = querystring.parse(decodeURIComponent(event.body))
+  console.log(params)
 
   const msg = `
-[Name]
-[Category]
-[LiveDate]
-[TicketsCount]
-[email]
+webサイトからContactがありました！
+
+--
+[Name] ${params.name}
+[Category] ${params.category}
+[LiveDate] ${params.liveDate}
+[TicketsCount] ${params.ticketsCount}
+[email] ${params.email}
 [Content]
-`
+${params.content}
+  `
 
   axios({
     method: 'post',
@@ -36,9 +33,6 @@ exports.handler = function(event, context, callback) {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     data: querystring.stringify({
-      // message: 'test string from "submittion-created"!'
-      // message: `本文： ${JSON.stringify(event.body)}`
-      // message: `本文： ${event.body.name}`
       message: msg
     })
   })
@@ -50,8 +44,4 @@ exports.handler = function(event, context, callback) {
   })
   .catch(err => callback(err))
 
-  // callback(null, {
-  //   statusCode: 200,
-  //   body: eventString
-  // })
 }

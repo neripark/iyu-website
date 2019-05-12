42f56bad253bc8ba6df99b4834b0b3dfcef3f231
neripark <neripark@gmail.com>
2019-05-12 23:31:31 +0900

    動画を軽いものに差し替え

--- components/MainVisual.vue
+++ components/MainVisual.vue
@@ -7,7 +7,7 @@ section.main-visual
     )
   information-summary
   video.bg-movie(
-    src="~assets/videos/190430_iyu-webtop-video-20sec3000bit24F.mp4"
+    src="~assets/videos/190430_iyu-webtop-video-5sec3000bit24F.mp4"
     muted
     autoplay
     loop

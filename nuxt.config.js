7b11dec4a9d4c2e5b03501e5f6fb884d9d8a24ef
neripark <neripark@gmail.com>
2019-05-19 00:29:53 +0900

    update ogp.

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,17 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '都内で活動中の新宿系お散歩ポップバンド、iyuの公式Webサイトです。バンドに関するすべての情報を掲載しています。' }
+      { hid: 'name', name: 'description', content: '都内で活動中の新宿系お散歩ポップバンド、iyuの公式Webサイトです。バンドに関するすべての情報を掲載しています。' },
+      { hid: 'og_url', property: 'og:url', content: 'https://iyumusic.tokyo' },
+      { hid: 'og_type', property: 'og:type', content: 'website' },
+      { hid: 'og_title', property: 'og:title', content: 'iyu' },
+      { hid: 'og_site_name', property: 'og:site_name', content: 'iyu' },
+      { hid: 'og_description', property: 'og:description', content: '都内で活動中の新宿系お散歩ポップバンド、iyuの公式Webサイトです。バンドに関するすべての情報を掲載しています。' },
+      // todo: 届いたら入れる
+      // { hid: 'og_image', property: 'og:image', content: '' },
+      // twitter
+      // { hid: 'og_twitter_site', property: 'twitter:site', content: '@iyu_band' },
+      // { hid: 'og_twitter_card', property: 'twitter:card', content: "summary_large_image" },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },

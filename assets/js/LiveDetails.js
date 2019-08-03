c67513b88df4a5b36e45ae354fef09284a334428
neripark <neripark@gmail.com>
2019-08-03 17:20:54 +0900

    update live info era

--- assets/js/LiveDetails.js
+++ assets/js/LiveDetails.js
@@ -1,17 +1,40 @@
 export default [
   {
-    title: 'Kichijoji NEPO Live Event',
+    title: 'ERA presents "MEAN TO YOU 〜DAY TIME SPECIAL〜"',
+    date: '2019/8/18 (Sat)',
+    place: '下北沢ERA ※お昼のイベントです。',
+    ticket: {
+      adv: 2400,
+      door: 2800,
+      other: '1drink(¥600)'
+    },
+    time: {
+      open: '12:30',
+      start: '12:50',
+      iyu: null
+    },
+    with: ['Of decay and sublime', '鎌野 愛', 'FLYING DEAD NYAN NYAN', 'MIRA']
+  },
+  {
+    title: 'Our Weekend',
     date: '2019/9/7 (Sat)',
     place: '吉祥寺NEPO ※夜のイベントです。',
     ticket: {
-      adv: null,
-      door: null
+      adv: 2000,
+      door: 2000,
+      other: '1order'
     },
     time: {
-      open: '未定',
-      start: '未定',
-      iyu: null
+      open: '17:30',
+      start: '18:00',
+      iyu: '18:00'
     },
-    with: ['パンとサーカス', 'ビオトープ', 'kawairitsu', 'and more...']
+    with: [
+      'パンとサーカス',
+      'ビオトープ',
+      'kawairitsu',
+      'アサカミドリ',
+      'ザ・フリーターズ'
+    ]
   }
 ]

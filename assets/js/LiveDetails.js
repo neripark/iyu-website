902eef13e3fa9626163d2d531f7ff56d58d5f18f
neripark <neripark@gmail.com>
2019-10-13 18:16:58 +0900

    kari

--- assets/js/LiveDetails.js
+++ assets/js/LiveDetails.js
@@ -1,32 +1,26 @@
 export default [
   {
-    title: 'ERA presents "MEAN TO YOU 〜DAY TIME SPECIAL〜"',
-    date: '2019/8/18 (Sun)',
-    place: '下北沢ERA ※お昼のイベントです。',
-    ticket: 'adv ¥2,400 / door ¥2,800 + 1drink(¥600) ',
+    title: 'FOYO presents "Daylight Mellow"',
+    date: '2019/11/3 (Sun)',
+    place: '下北沢 mona records ※お昼のイベントです。',
+    ticket: 'Lunch Set ¥3,200 / 1drink ¥2,800',
     time: {
-      open: '12:30',
-      start: '12:50',
-      iyu: null
+      open: '12:00',
+      start: '12:30',
+      iyu: '12:30'
     },
-    with: ['Of decay and sublime', '鎌野 愛', 'FLYING DEAD NYAN NYAN', 'MIRA']
+    with: ['FOYO', 'イハラカンタロウ楽団']
   },
   {
-    title: 'Our Weekend',
-    date: '2019/9/7 (Sat)',
-    place: '吉祥寺NEPO ※夜のイベントです。',
-    ticket: '¥2,000 + 1order',
+    title: 'NEPO presents 〜Our Weekend〜',
+    date: '2019/11/10 (Sun)',
+    place: '吉祥寺NEPO',
+    ticket: 'adv / door ¥2,000 (＋1d)',
     time: {
       open: '17:30',
       start: '18:00',
-      iyu: '18:00'
+      iyu: '21:00'
     },
-    with: [
-      'パンとサーカス',
-      'ビオトープ',
-      'kawairitsu',
-      'アサカミドリ',
-      'ザ・フリーターズ'
-    ]
+    with: [ 'adult cinema', 'ReeSyant', 'The Bedroom Cloud', 'The 1st Sunday', 'ドラリ座']
   }
 ]

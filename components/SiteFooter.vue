f8007c34aa65bf2b36ce2b529d5e224afbf0736f
neripark <neripark@gmail.com>
2019-10-14 17:51:02 +0900

    delete unnecessary comment

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,77 +1,24 @@
 <template lang="pug">
 footer.site-footer
-  //- .wrap-mail
-    //- todo: メールへの導線をデザインから再設定
-    //- p.mail-img
-    //-   img.mail-icon(
-    //-     src="~/assets/images/mail-icon.svg"
-    //-     alt="mail"
-    //-   )
-    //- .text
-    //-   p.mail-text メールでのお問い合わせはこちらまでお願いいたします。
-    //-   p.mail-text iyumusictokyo@gmail.com
   .wrap-copyright
     p.copyright-text ©iyumusic.tokyo All Rights Reserved.
-  //- todo: SNSへの導線をデザインから再設定
-  //- .wrap-sns-icons
-  //-   a(
-  //-     href="https://twitter.com/iyu_band"
-  //-     target="_blank"
-  //-   )
-  //-     img.twitter-icon(
-  //-       src="~/assets/images/twitter-icon.svg"
-  //-       alt="mail"
-  //-     )
 </template>
 
 <style lang="scss" scoped>
 * {
   color: $white;
 }
-// img
-// .mail-icon {
-//   height: 50px;
-//   width: auto;
-// }
-// .twitter-icon {
-//   height: 40px;
-//   width: auto;
-// }
-// styles
 .site-footer {
   width: 100%;
-  // padding: 20px 40px;
   padding: 40px $side-padding-pc;
   @include mq() {
     padding: 30px $side-padding-sp;
   }
   background-color: $main-blue;
-  // display: flex;
-  // justify-content: space-between;
   border-top: 1px solid $white;
 }
-// .wrap-mail {
-//   display: flex;
-//   justify-content: flex-start;
-//   align-items: center;
-//   width: 33%;
-//   font-size: 14px;
-//   .text {
-//     margin-left: 15px;
-//   }
-// }
 .wrap-copyright {
-  // display: inline-flex;
-  // justify-content: center;
-  // align-items: center;
-  // width: 33%;
   text-align: center;
   font-size: 12px;
 }
-// .wrap-sns-icons {
-//   display: inline-flex;
-//   justify-content: flex-end;
-//   align-items: center;
-//   width: 33%;
-// }
 </style>

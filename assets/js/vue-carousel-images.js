a78d2428c5e75a68c14b9c9c3cb52dd742b36f61
neripark <neripark@gmail.com>
2020-01-05 18:35:06 +0900

    use v-for for gallery images

---
+++ assets/js/vue-carousel-images.js
@@ -0,0 +1,21 @@
+const carouselImages = [
+  {
+    // nuxtに解釈させるためにrequire
+    src: require('~/assets/images/gallery-sky.jpg'),
+    alt: 'gallery sky'
+  },
+  {
+    src: require('~/assets/images/gallery-setasu-last.jpg'),
+    alt: 'setagaya suside last scene'
+  },
+  {
+    src: require('~/assets/images/gallery-car-beam.jpg'),
+    alt: 'gallery car beam'
+  },
+  {
+    src: require('~/assets/images/gallery-car-light.jpg'),
+    alt: 'gallery car light'
+  }
+];
+
+export default carouselImages;

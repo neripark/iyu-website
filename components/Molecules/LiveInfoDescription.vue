a081369e29fc39f7ef0a06f53599b0e825767c8e
neripark <neripark@gmail.com>
2022-05-01 16:46:35 +0900

    fix for lint and adjust word space

--- components/Molecules/LiveInfoDescription.vue
+++ components/Molecules/LiveInfoDescription.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
     <p v-if="liveCnt" class="lead">
-      チケットのお取置きは
-      <a
+      <!-- eslint-disable vue/max-attributes-per-line vue/html-indent -->
+      <span>チケットのお取置きは</span
+      ><a
         v-scroll-to="scrollTo(anchorList.contact)"
         class="text-link"
         :href="`#${anchorList.profile.id}`"
-      >
-        こちらのフォーム</a
-      >
-      から。<br />
-      <a class="text-link" href="https://twitter.com/iyu_band" target="_blank">
-        Twitter
-      </a>
-      でも承っております。
+        >こちらのフォーム</a
+      >から。<br />
+      <a class="text-link" href="https://twitter.com/iyu_band" target="_blank"
+        >Twitter</a
+      >でも承っております。
     </p>
     <p v-if="!liveCnt" class="lead">
       現在予定しているライブはありません。
       <br />発表をお待ち下さい！<br />
-      <a class="text-link" href="https://twitter.com/iyu_band" target="_blank">
-        Twitter </a
+      <a class="text-link" href="https://twitter.com/iyu_band" target="_blank"
+        >Twitter</a
       >もぜひチェックしてくださいね。
     </p>
+    <!-- eslint-enable -->
   </div>
 </template>
 
@@ -80,7 +79,7 @@ export default {
 .text-link {
   color: $white;
   text-decoration: underline;
-  margin: 0 0.2em;
+  margin: 0 0.1em;
   @include selection-color();
   &:hover {
     text-decoration: none;
